43278f372d3ad4aef3e66ef087ed9ab9f5817494
Juliana <juliana6k@mail.ru>
2024-04-17 16:43:21 +0300

    шаблоны, views

--- mail/urls.py
+++ mail/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from mail.views import MainPageView, MailingListView
+from mail.views import MainPageView, MailingListView, MailingCreateView, MailingUpdateView, \
+    MailingDetailView, MailingDeleteView
 from django.views.decorators.cache import cache_page
 from django.conf.urls.static import static
 from django.conf import settings
@@ -11,6 +12,9 @@ app_name = 'mailing'
 urlpatterns = [path('', cache_page(60)(MainPageView.as_view()), name='main_page'),
                path('mailinglist/', MailingListView.as_view(), name='mailing_list'),
                path('details/<int:pk>/', MailingDetailView.as_view(), name='mailing_details'),
+               path('mailingcreate/', MailingCreateView.as_view(), name='mailing_create'),
+               path('mailingupdate/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+               path('mailingdelete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
      ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- mail/views.py
+++ mail/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, \
     TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from mail.models import Mailing, Client, Message, Mail_attempt
 from blog.models import Post
+from mail.forms import MailingForm
+from django.urls import reverse_lazy, reverse
 def index(request):
     return render(request, 'mail/index.html')
 
@@ -18,6 +21,38 @@ class MailingDetailView(LoginRequiredMixin, DetailView):
     """Просмотр рассылки по id"""
     model = Mailing
 
+
+class MailingCreateView(LoginRequiredMixin, CreateView):
+    """Создание рассылки"""
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
+    form = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+  def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        user = self.request.user
+        if user.groups.filter(name='Менеджер').exists() or user.is_superuser:
+            return self.object
+        if user != self.object.author:
+            raise Http404("Вы можете редактировать только свои рассылки")
+        return self.object
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    model = Mailing
+    success_url = reverse_lazy('mail:mailing_list')
 
 class MainPageView(TemplateView):
     """Отображение главной страницы сервиса"""
