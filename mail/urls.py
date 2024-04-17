a025f6465e2c25648eeb9caf9079f8bc9ece2de8
Juliana <juliana6k@mail.ru>
2024-04-18 01:24:23 +0300

    шаблоны, views

--- mail/urls.py
+++ mail/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from mail.views import MainPageView, MailingListView, MailingCreateView, MailingUpdateView, \
     MailingDetailView, MailingDeleteView
 from mail.views import ClientListView, ClientCreateView, ClientUpdateView, ClientDeleteView, \
-    MessageCreateView, MessageListView
+    MessageCreateView, MessageListView, MailAttemptListView
 
 from django.views.decorators.cache import cache_page
 from django.conf.urls.static import static
@@ -20,6 +20,7 @@ urlpatterns = [path('', cache_page(60)(MainPageView.as_view()), name='main_page'
                path('mailingdelete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
                path('messagelist/', MessageListView.as_view(), name='message_list'),
                path('messagecreate/', MessageCreateView.as_view(), name='message_create'),
+               path('mailattemptlist/', MailAttemptListView.as_view(), name='mailattempt_list'),
                path('clientlist/', ClientListView.as_view(), name='client_list'),
                path('clientcreate/', ClientCreateView.as_view(), name='client_create'),
                path('clientupdate<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
